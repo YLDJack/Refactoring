32523a72064c76e28137949a1e5941f50fcd39f0
yulunduan <yulunduan@corp.netease.com>
2022-02-06 15:40:58 +0800

    feat(重构名录):11.拆分阶段（Split Phase）

---
+++ 重构名录/11.拆分阶段（Split Phase）/2.范例.js
@@ -0,0 +1,63 @@
+function priceOrder(product, quantity, shippingMethod) {
+  //前两行代码根据商品（product） 信息计算订单中与商品相关的价格，
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  //随后的两行则根据配送 （shipping）信息计算配送成本。
+  const shippingPerCase =
+    basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFee
+      : shippingMethod.feePerCase;
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+  return price;
+}
+
+//处理完参数列表后，中转数据结构得到了完整的填充.现在我可以把第一阶段代码提炼成独立的函数，令其返回这 份数据
+function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  const price = applyShipping(priceData, shippingMethod);
+  return price;
+}
+function calculatePricingData(product, quantity) {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  //中转数据结构得到了完整的填充.现在我可以把第一阶段代码提炼成独立的函数，令其返回这 份数据
+  return { basePrice: basePrice, quantity: quantity, discount: discount };
+}
+function applyShipping(priceData, shippingMethod) {
+  const shippingPerCase =
+    priceData.basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFee
+      : shippingMethod.feePerCase;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
+  return price;
+}
+
+//两个函数中，最后一个const变量都是多余的，我忍不住 洁癖，将它们内联消除掉。
+function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  return applyShipping(priceData, shippingMethod);
+}
+function calculatePricingData(product, quantity) {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  return { basePrice: basePrice, quantity: quantity, discount: discount };
+}
+function applyShipping(priceData, shippingMethod) {
+  const shippingPerCase =
+    priceData.basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFee
+      : shippingMethod.feePerCase;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  return priceData.basePrice - priceData.discount + shippingCost;
+}
