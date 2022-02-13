375161198ff6980a5210ab763e477af2cf056c09
YLDJack <779618724@qq.com>
2022-02-13 16:56:08 +0800

    feat(搬移特性):7.拆分循环（Split Loop）

--- 八、搬移特性/7 拆分循环（Split Loop）/1.示例.js
+++ 八、搬移特性/7 拆分循环（Split Loop）/1.示例.js
@@ -1,8 +1,17 @@
-manager = aPerson.department.manager;
-//=========================================>
-manager = aPerson.manager;
-class Person {
-  get manager() {
-    return this.department.manager;
-  }
+let averageAge = 0;
+let totalSalary = 0;
+for (const p of people) {
+  averageAge += p.age;
+  totalSalary += p.salary;
 }
+averageAge = averageAge / people.length;
+//=========================================>
+let totalSalary = 0;
+for (const p of people) {
+  totalSalary += p.salary;
+}
+let averageAge = 0;
+for (const p of people) {
+  averageAge += p.age;
+}
+averageAge = averageAge / people.length;
--- 八、搬移特性/7 拆分循环（Split Loop）/2.范例.js
+++ 八、搬移特性/7 拆分循环（Split Loop）/2.范例.js
@@ -1,40 +1,74 @@
-//有些客户端希望知道某人的经理是谁，为此，它必须先 取得Department对象。
-manager = aPerson.department.manager;
+//下面我以一段循环代码开始。该循环会计算需要支付给 所有员工的总薪水（total salary），
+//并计算出最年轻 （youngest）员工的年龄。
+let youngest = people[0] ? people[0].age : Infinity;
+let totalSalary = 0;
+for (const p of people) {
+  if (p.age < youngest) youngest = p.age;
+  totalSalary += p.salary;
+}
+return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
 
-class Person {
-  constructor(name) {
-    this._name = name;
+//这两种计算，我要先复制一遍循环代码。
+let youngest = people[0] ? people[0].age : Infinity;
+let totalSalary = 0;
+for (const p of people) {
+  if (p.age < youngest) youngest = p.age;
+  totalSalary += p.salary;
+}
+for (const p of people) {
+  if (p.age < youngest) youngest = p.age;
+  totalSalary += p.salary;
+}
+return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+
+//复制过后，我需要将循环中重复的计算逻辑删除，否则 就会累加出错误的结果。如果循环中的代码没有副作用，那 便可以先留着它们不删除，可惜上述例子并不符合这种情 况
+let youngest = people[0] ? people[0].age : Infinity;
+let totalSalary = 0;
+for (const p of people) {
+  totalSalary += p.salary;
+}
+for (const p of people) {
+  if (p.age < youngest) youngest = p.age;
+}
+return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+
+//至此，拆分循环这个手法本身的内容就结束了。
+//但本手 法的意义不仅在于拆分出循环本身，而且在于它为进一步优 化提供了良好的起点——
+//下一步我通常会寻求将每个循环提 炼到独立的函数中。
+//在做提炼之前，我得先用移动语句 （223）微调一下代码顺序，将与循环相关的变量先搬移到 一起：
+let totalSalary = 0;
+for (const p of people) {
+  totalSalary += p.salary;
+}
+let youngest = people[0] ? people[0].age : Infinity;
+for (const p of people) {
+  if (p.age < youngest) youngest = p.age;
+}
+return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+
+//然后，我就可以顺利地应用提炼函数（106）了。
+return `youngestAge: ${youngestAge()}, totalSalary: ${totalSalary()}`;
+function totalSalary() {
+  let totalSalary = 0;
+  for (const p of people) {
+    totalSalary += p.salary;
   }
-  get name() {
-    return this._name;
+  return totalSalary;
+}
+function youngestAge() {
+  let youngest = people[0] ? people[0].age : Infinity;
+  for (const p of people) {
+    if (p.age < youngest) youngest = p.age;
   }
-  get department() {
-    return this._department;
-  }
-  set department(arg) {
-    this._department = arg;
-  }
-  //这样的编码就对客户端揭露了Department的工作原理， 于是客户知道：Department负责追踪“经理”这条信息。如 果对客户隐藏Department，可以减少耦合。为了这一目的， 我在Person中建立一个简单的委托函数
-  get manager() {
-    return this._department.manager;
-  }
+  return youngest;
 }
 
-class Department {
-  get chargeCode() {
-    return this._chargeCode;
-  }
-  set chargeCode(arg) {
-    this._chargeCode = arg;
-  }
-  get manager() {
-    return this._manager;
-  }
-  set manager(arg) {
-    this._manager = arg;
-  }
-}
+//对于像totalSalary这样的累加计算，我绝少能抵挡得住 进一步使用以管道取代循环（231）重构它的诱惑；而对于 youngestAge的计算，显然可以用替换算法（195）替之以更 好的算法。
 
-//现在，我得修改Person的所有客户端，让它们改用新函 数
-//只要完成了对Department所有函数的修改，并相应修改 了Person的所有客户端，我就可以移除Person中的department 访问函数了。
-manager = aPerson.manager;
+return `youngestAge: ${youngestAge()}, totalSalary: ${totalSalary()}`;
+function totalSalary() {
+  return people.reduce((total, p) => total + p.salary, 0);
+}
+function youngestAge() {
+  return Math.min(...people.map((p) => p.age));
+}
