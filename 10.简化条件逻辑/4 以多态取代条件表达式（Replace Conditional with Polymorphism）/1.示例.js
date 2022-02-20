0d6656a38099a19676f19999ee3a81efef207649
YLDJack <779618724@qq.com>
2022-02-20 15:03:58 +0800

    feat(简化条件逻辑):4 以多态取代条件表达式（Replace Conditional with Polymorphism）

--- 10.简化条件逻辑/4 以多态取代条件表达式（Replace Conditional with Polymorphism）/1.示例.js
+++ 10.简化条件逻辑/4 以多态取代条件表达式（Replace Conditional with Polymorphism）/1.示例.js
@@ -1,12 +1,27 @@
-class Product {
-  applyDiscount(arg) {
-    this._price.amount -= arg;
-  }
+switch (bird.type) {
+  case "EuropeanSwallow":
+    return "average";
+  case "AfricanSwallow":
+    return bird.numberOfCoconuts > 2 ? "tired" : "average";
+  case "NorwegianBlueParrot":
+    return bird.voltage > 100 ? "scorched" : "beautiful";
+  default:
+    return "unknown";
 }
 //=========================================>
 
-class Product {
-  applyDiscount(arg) {
-    this._price = new Money(this._price.amount - arg, this._price.currency);
+class EuropeanSwallow {
+  get plumage() {
+    return "average";
+  }
+}
+class AfricanSwallow {
+  get plumage() {
+    return this.numberOfCoconuts > 2 ? "tired" : "average";
+  }
+}
+class NorwegianBlueParrot {
+  get plumage() {
+    return this.voltage > 100 ? "scorched" : "beautiful";
   }
 }
