effbc2bbbd5c4ce6c1bbd55d666742b4770b7014
YLDJack <779618724@qq.com>
2022-02-21 22:39:10 +0800

    feat(重构API): 4 保持对象完整（Preserve Whole Object）

--- 11.重构API/4 保持对象完整（Preserve Whole Object）/1.示例.js
+++ 11.重构API/4 保持对象完整（Preserve Whole Object）/1.示例.js
@@ -1,27 +1,4 @@
-switch (bird.type) {
-  case "EuropeanSwallow":
-    return "average";
-  case "AfricanSwallow":
-    return bird.numberOfCoconuts > 2 ? "tired" : "average";
-  case "NorwegianBlueParrot":
-    return bird.voltage > 100 ? "scorched" : "beautiful";
-  default:
-    return "unknown";
-}
+const low = aRoom.daysTempRange.low; const high = aRoom.daysTempRange.high; if (aPlan.withinRange(low, high))
 //=========================================>
 
-class EuropeanSwallow {
-  get plumage() {
-    return "average";
-  }
-}
-class AfricanSwallow {
-  get plumage() {
-    return this.numberOfCoconuts > 2 ? "tired" : "average";
-  }
-}
-class NorwegianBlueParrot {
-  get plumage() {
-    return this.voltage > 100 ? "scorched" : "beautiful";
-  }
-}
+if (aPlan.withinRange(aRoom.daysTempRange))
