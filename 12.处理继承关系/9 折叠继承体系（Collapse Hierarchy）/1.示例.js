1bb4cafd4153c5382912db2b4d064ef48c5ee43f
YLDJack <779618724@qq.com>
2022-03-03 22:42:43 +0800

    feat(处理继承关系): 9 折叠继承体系（Collapse Hierarchy）

--- 12.处理继承关系/9 折叠继承体系（Collapse Hierarchy）/1.示例.js
+++ 12.处理继承关系/9 折叠继承体系（Collapse Hierarchy）/1.示例.js
@@ -1,16 +1,4 @@
-function score(candidate, medicalExam, scoringGuide) {
-  let result = 0;
-  let healthLevel = 0; // long body code
-}
+class Employee {}
+class Salesman extends Employee {}
 //=========================================>
-class Scorer {
-  constructor(candidate, medicalExam, scoringGuide) {
-    this._candidate = candidate;
-    this._medicalExam = medicalExam;
-    this._scoringGuide = scoringGuide;
-  }
-  execute() {
-    this._result = 0;
-    this._healthLevel = 0; // long body code
-  }
-}
+class Employee {}
